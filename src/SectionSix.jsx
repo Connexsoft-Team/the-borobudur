1008d31d66a3724c5815348337ee366406fa81b6
Abraham <athioii6@gmail.com>
2024-02-16 15:33:03 +0700

    add animation in section six

--- src/SectionSix.jsx
+++ src/SectionSix.jsx
@@ -13,17 +13,85 @@ export default function SectionSix() {
  useEffect(() => {
   gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to(".text-loc", {
+    gsap.to(".text-icon", {
       opacity: 1,
-      // ease: "bounce",
       scrollTrigger: {
-        trigger: ".gambar-b",
+        trigger: ".text-visit",
+        scrub: true,
+        markers: true,
+        start: "top center",
+        end:"bottom center",
+      }
+    })
+
+    gsap.to(".text-loc-1", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".text-icon",
         scrub: true,
         markers: true,
         start: "top center",
         end:"bottom center",
       }
     });
+
+    gsap.to(".text-desc-1", {
+      opacity: 0.7,
+      scrollTrigger: {
+        trigger: ".text-loc-1",
+        scrub: true,
+        markers: true,
+        start: "top center",
+        end:"bottom center",
+      }
+    });
+
+    
+    gsap.to(".text-loc-2", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".text-icon",
+        scrub: true,
+        markers: true,
+        start: "top center",
+        end:"bottom center",
+      }
+    });
+
+    gsap.to(".text-desc-2", {
+      opacity: 0.7,
+      scrollTrigger: {
+        trigger: ".text-loc-2",
+        scrub: true,
+        markers: true,
+        start: "top center",
+        end:"bottom center",
+      }
+    });
+
+    gsap.to(".text-loc-3", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".text-icon",
+        scrub: true,
+        markers: true,
+        start: "top center",
+        end:"bottom center",
+      }
+    });
+
+    gsap.to(".text-desc-3", {
+      opacity: 0.7,
+      scrollTrigger: {
+        trigger: ".text-loc-1",
+        scrub: true,
+        markers: true,
+        start: "top center",
+        end:"bottom center",
+      }
+    });
+
+
   }, []);
 
   return (
@@ -34,7 +102,7 @@ export default function SectionSix() {
           alt="none"
           className="w-[400px] h-[300px] object-cover"
         />
-        <span id="text1" className="text-[48px] text-white font-bold mt-[32px]">
+        <span id="text1" className="text-visit text-[48px] text-white font-bold mt-[32px]">
           Visit The Temple
         </span>
         <span className="text-[16px] text-[#E4E1D4] mb-[32px]" id="mulai">
@@ -45,33 +113,39 @@ export default function SectionSix() {
             <img
               src={compas}
               alt="none"
-              className="gambar-b w-[67px] h-[67px] object-cover"
+              className="text-icon w-[67px] h-[67px] object-cover"
+              style={{ opacity: 0 }}
             />
-            <span className="text-loc text-[48px] text-white font-bold my-[16px] opacity-0">
+            <span className="text-loc-1 text-[48px] text-white font-bold my-[16px]" style={{ opacity: 0 }}>
               Location
             </span>
-            <span className="text-[16px] text-[#E4E1D4] text-center opacity-70">
-              The closest major cities to Borobudur, with international flights,
-              are Semarang and Yogyakarta. These two cities accommodate flight
-              routes from Kuala Lumpur and Singapore, but Yogyakarta (with a
-              distance of 40 km from Borobudur) is closer than Semarang (with a
-              distance of 90 km).
-            </span>
-            <span className="text-[16px] text-[#E4E1D4] text-center mt-[16px] opacity-70">
-              The other closest city to Borobudur is Magelang with a distance of
-              17 km, which has a direct bus route to Borobudur.
-            </span>
+            <div className=" text-center">
+              <span className="text-desc-1 text-[16px] text-[#E4E1D4] text-center" style={{ opacity: 0 }}>
+                The closest major cities to Borobudur, with international flights,
+                are Semarang and Yogyakarta. These two cities accommodate flight
+                routes from Kuala Lumpur and Singapore, but Yogyakarta (with a
+                distance of 40 km from Borobudur) is closer than Semarang (with a
+                distance of 90 km).
+              </span>
+              <br />
+              <br />
+              <span className="text-desc-1 text-[16px] text-[#E4E1D4] text-center mt-[16px]" style={{ opacity: 0 }}>
+                The other closest city to Borobudur is Magelang with a distance of
+                17 km, which has a direct bus route to Borobudur.
+              </span>
+            </div>
           </div>
           <div className="flex flex-col bg-[#302E2D] p-[32px] rounded-[16px] w-full items-center">
             <img
               src={time}
               alt="none"
-              className="w-[51px] h-[71px] object-cover"
+              className="text-icon w-[51px] h-[71px] object-cover"
+              style={{ opacity: 0 }}
             />
-            <span className="text-[48px] text-white font-bold my-[16px]">
+            <span className="text-loc-2 text-[48px] text-white font-bold my-[16px]" style={{ opacity: 0 }}>
               Hours
             </span>
-            <span className="text-[16px] text-[#E4E1D4] text-center opacity-70">
+            <span className="text-desc-2 text-[16px] text-[#E4E1D4] text-center" style={{ opacity: 0 }}>
               Every Tuesday-Sunday, visitors can visit Zone 1 of Borobudur
               Temple (temple grounds/yard) at 08.30 - 15.30 WIB. It is not
               permitted to enter the room / climb the temple building. Every
@@ -83,29 +157,31 @@ export default function SectionSix() {
             <img
               src={telephone}
               alt="none"
-              className="w-[63px] h-[64px] object-cover"
+              className="text-icon w-[63px] h-[64px] object-cover"
+              style={{ opacity: 0 }}
             />
-            <span className="text-[48px] text-white font-bold my-[16px]">
+            <span className="text-loc-3 text-[48px] text-white font-bold my-[16px]" style={{ opacity: 0 }}>
               Contact
             </span>
-            <span className="text-[16px] text-[#E4E1D4] text-center opacity-70">
+            <span className="text-desc-3 text-[16px] text-[#E4E1D4] text-center" style={{ opacity: 0 }}>
               Step into a realm where ancient whispers echo through time, where
               each stone tells a story of a bygone era — welcome to the
               enigmatic world of Borobudur.
             </span>
             <div className="flex flex-col gap-[16px] mt-[16px] items-center">
-              <span className="text-[16px] text-[#E4E1D4] font-bold">
+              <span className="text-desc-3 text-[16px] text-[#E4E1D4] font-bold" style={{ opacity: 0 }}>
                 +62 811-2688-000
               </span>
               <img
                 src={envelope}
                 alt="none"
-                className="w-[22px] h-[18px] object-cover"
+                className="text-desc-3 w-[22px] h-[18px] object-cover"
+                style={{ opacity: 0 }}
               />
-              <span className="text-[16px] text-[#E4E1D4] font-bold">
+              <span className="text-desc-3 text-[16px] text-[#E4E1D4] font-bold" style={{ opacity: 0 }}>
                 marketing@borobudurpark.co.id
               </span>
-              <span className="text-[16px] text-[#E4E1D4] font-bold">
+              <span className="text-desc-3 text-[16px] text-[#E4E1D4] font-bold" style={{ opacity: 0 }}>
                 info@borobudurpark.co.id
               </span>
             </div>
