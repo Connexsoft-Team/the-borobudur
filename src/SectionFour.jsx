0678ae203997b91cfa3a696ee047a46140a3e1fb
Raya46 <muhammadrayaarrizki@gmail.com>
2024-02-18 00:02:45 +0700

    animation gallery and animation sectiontwo

--- src/SectionFour.jsx
+++ src/SectionFour.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import image1 from "./assets/image1.png";
 import image2 from "./assets/image2.png";
 import image3 from "./assets/image3.png";
@@ -9,13 +11,47 @@ import image6 from "./assets/image6.png";
 import image7 from "./assets/image7.png";
 
 export default function SectionFour() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(".wrapper-4", {
+      x: "-100%",
+      scrollTrigger: {
+        trigger: ".wrapper-trig",
+        start: "top center",
+        end: "bottom center",
+        scrub: 0.3,
+        markers: true,
+      },
+    });
+
+    gsap.to(".wrapper-5", {
+      x: "100%",
+      scrollTrigger: {
+        trigger: ".wrapper-trig",
+        start: "top center",
+        end: "bottom center",
+        scrub: 0.3,
+        markers: true,
+      },
+    });
+    gsap.to(".wrapper-6", {
+      x: "-120%",
+      scrollTrigger: {
+        trigger: ".wrap-t",
+        start: "top center",
+        end: "bottom center",
+        scrub: 0.3,
+        markers: true,
+      },
+    });
+  }, []);
   return (
-    <div className="w-full h-full">
-      <div className="flex flex-col items-center gap-[8px] overflow-x-hidden">
+    <div className="wrap-t w-full h-full relative overflow-hidden">
+      <div className="wrapper-trig flex flex-col items-center gap-[10px]">
         <span className="text-[#E4E1D4] text-[54px] lg:text-[64px] font-bold">
           GALLERY
         </span>
-        <div className="flex lg:flex-row flex-col gap-[8px]">
+        <div className="wrapper-4 flex lg:flex-row flex-col gap-[10px]">
           <img
             src={image7}
             alt="image 1"
@@ -26,8 +62,43 @@ export default function SectionFour() {
             alt="image 2"
             className="object-cover w-full h-[453px]"
           />
+          <img
+            src={image5}
+            alt="image 2"
+            className="object-cover w-full h-[453px]"
+          />
+          <img
+            src={image4}
+            alt="image 2"
+            className="object-cover w-full h-[453px]"
+          />
+          <img
+            src={image2}
+            alt="image 2"
+            className="object-cover w-full h-[453px]"
+          />
         </div>
-        <div className="flex gap-[8px]">
+        <div className="wrapper-5 flex flex-col lg:flex-row-reverse gap-[10px]">
+          <img
+            src={image4}
+            alt="image 3"
+            className="object-cover w-full h-[453px] lg:block hidden"
+          />
+          <img
+            src={image1}
+            alt="image 3"
+            className="object-cover w-full h-[453px] lg:block hidden"
+          />
+           <img
+            src={image2}
+            alt="image 3"
+            className="object-cover w-full h-[453px] lg:block hidden"
+          />
+          <img
+            src={image7}
+            alt="image 3"
+            className="object-cover w-full h-[453px] lg:block hidden"
+          />
           <img
             src={image5}
             alt="image 1"
@@ -39,12 +110,22 @@ export default function SectionFour() {
             className="object-cover w-full h-[453px]"
           />
           <img
-            src={image2}
+            src={image7}
+            alt="image 3"
+            className="object-cover w-full h-[453px] lg:block hidden"
+          />
+          <img
+            src={image6}
             alt="image 3"
             className="object-cover w-full h-[453px] lg:block hidden"
           />
         </div>
-        <div className="flex gap-[8px]">
+        <div className="wrapper-6 flex flex-col lg:flex-row gap-[10px]">
+          <img
+            src={image7}
+            alt="image 1"
+            className="object-cover w-full h-[453px]"
+          />
           <img
             src={image4}
             alt="image 1"
@@ -52,6 +133,16 @@ export default function SectionFour() {
           />
           <img
             src={image3}
+            alt="image 2"
+            className="object-cover w-full h-[453px]"
+          />
+          <img
+            src={image6}
+            alt="image 2"
+            className="object-cover w-full h-[453px]"
+          />
+          <img
+            src={image7}
             alt="image 2"
             className="object-cover w-full h-[453px]"
           />
--- src/SectionTwo.jsx
+++ src/SectionTwo.jsx
@@ -1,31 +1,38 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect } from "react";
-import sectiontwoimg from "./assets/sectiontwoimg.png"
-import { gsap } from "gsap"
+import sectiontwoimg from "./assets/sectiontwoimg.png";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
-
 export default function SectionTwo() {
-
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-
-    gsap.from("#image-cover", {
-      opacity: 0,
-      y: 50,
+    gsap.to(".world-heritage", {
+      opacity: 1,
       duration: 1,
-      ease: "power1.inOut",
-      delay: 1,
+      ease: "power2.out",
       scrollTrigger: {
-        trigger: "#image-cover",
-        markers: true,
+        trigger: ".world-heritage",
         start: "top center",
         end: "bottom center",
-        scrub: true
+        scrub: true,
+        markers: true, // You can remove this in production
       },
     });
 
+    gsap.to(".random-text", {
+      opacity: 1,
+      stagger: 0.1, // Adjust the stagger value as needed
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: ".random-text",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        markers: true, // You can remove this in production
+      },
+    });
   }, []);
 
   return (
@@ -41,10 +48,10 @@ export default function SectionTwo() {
           </div>
         </div>
         <div className="flex flex-col w-full">
-          <span id="secondary" className="text-[48px] md:text-[60px] lg:text-[96px] leading-normal lg:leading-[96px] font-bold text-[#E4E1D4]">
+          <span style={{ opacity:0 }} className="text-[48px] md:text-[60px] lg:text-[96px] leading-normal lg:leading-[96px] font-bold text-[#E4E1D4] world-heritage">
             World Heritage
           </span>
-          <span id="text-animate" className="text-white/60 text-[16px] mt-[1rem] lg:mt-[2rem] font-semibold secondary">
+          <span style={{ opacity:0 }} className="text-white/60 text-[16px] mt-[1rem] lg:mt-[2rem] font-semibold random-text">
             Between 780-840 AD, during the reign oaf the Sailendra dynasty, the grandest Buddhist relic ever known to mankind was erected. Serving both as a sanctuary for Buddhist devotion and a sacred pilgrimage site, this monumental structure held within its walls teachings guiding individuals to transcend worldly desires and embrace the path to enlightenment and wisdom as elucidated by the Buddha. In 1814, under the command of Sir Thomas Stanford Raffles, British troops stumbled upon this marvel, lost to time until then. It wasn't until 1835 that efforts were made to meticulously cleanse and restore the temple grounds to their former glory.
           </span>
         </div>
