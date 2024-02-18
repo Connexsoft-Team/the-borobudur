9ed8275e766434af8bbbbdae27d844c23aa3fb08
Raya46 <muhammadrayaarrizki@gmail.com>
2024-02-18 14:11:13 +0700

    add smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 /* eslint-disable no-unused-vars */
 import ParallaxSection from "./parralax"
 import SectionFour from "./SectionFour";
-import SectionOne from "./SectionOne";
 import SectionSix from "./SectionSix";
 import SectionThree from "./SectionThree";
 import SectionTwo from "./SectionTwo";
 import Footer from "./components/Footer";
 import SectionLevelBorobudur from "./components/SectionBorobudurLevels";
-
+import {ReactLenis} from "@studio-freight/react-lenis"
 
 function App() {
   return (
-    <>
+    <ReactLenis root>
       <ParallaxSection />
       <SectionTwo />
       <SectionThree />
@@ -19,9 +18,7 @@ function App() {
       <SectionFour />
       <SectionSix />
       <Footer />
-      {/* <SectionOne/> */}
-      {/* <SectionSix/> */}
-    </>
+    </ReactLenis>
   );
 }
 
