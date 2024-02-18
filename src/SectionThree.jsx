2c4d31e1635c4d5b4f8cc0a0bf4360f95f543cff
rafaelrnzo <rl.lorenzo.256@gmail.com>
2024-02-18 17:29:36 +0700

    fix section three

--- src/SectionThree.jsx
+++ src/SectionThree.jsx
@@ -10,7 +10,8 @@ export default function SectionThree() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
     gsap.to(".wrapper-1", {
-      x: "60%",
+      x: "80%",
+      duration: 30,
       scrollTrigger: {
         trigger: ".wrapper-trigger",
         start: "top center",
@@ -20,7 +21,8 @@ export default function SectionThree() {
     });
 
     gsap.to(".wrapper-2", {
-      x: "-60%",
+      x: "-80%",
+      duration: 30,
       scrollTrigger: {
         trigger: ".wrapper-trigger",
         start: "top center",
@@ -31,7 +33,7 @@ export default function SectionThree() {
   }, []);
 
   return (
-    <div className="w-screen relative overflow-hidden h-full">
+    <div className="w-screen relative overflow-hidden h-full transition-all duration-700">
       <div className="wrapper-trigger flex flex-col max-w-screen mt-[10rem] mb-[64px] gap-[16px]">
         <div className="wrapper-1 inline-flex flex-row-reverse flex-nowrap max-w-screen items-center gap-[32px]">
           <img
@@ -52,8 +54,44 @@ export default function SectionThree() {
             className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
           />
           <p className="text-white text-[96px] font-bold">ART</p>
+          <img
+            src={borobudur}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover"
+          />
+          <span className="stroke-text">ARCHITECTURE</span>
+          <img
+            src={komang}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <p className="text-white text-[96px] font-bold">CULTURES</p>
+          <img
+            src={ukiran}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <p className="text-white text-[96px] font-bold">ART</p>
         </div>
         <div className="wrapper-2 inline-flex flex-nowrap max-w-screen items-center gap-[32px] lg:-ml-0 -ml-[220px]">
+          <img
+            src={ukiran}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <p className="text-white text-[96px] font-bold">ART</p>
+          <img
+            src={borobudur}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover"
+          />
+          <p className="stroke-text">ARCHITECTURE</p>
+          <img
+            src={komang}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <p className="text-white text-[96px] font-bold">CULTURES</p>
           <img
             src={ukiran}
             alt="none"
--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -47,6 +47,24 @@ export default function SectionThree() {
             className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
           />
           <p className="text-white text-[96px] font-bold">CULTURES</p>
+          <img
+            src={ukiran}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <p className="text-white text-[96px] font-bold">ART</p>
+          <img
+            src={borobudur}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover"
+          />
+          <p className="stroke-text">ARCHITECTURE</p>
+          <img
+            src={komang}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <p className="text-white text-[96px] font-bold">CULTURES</p>
         </div>
       </div>
     </div>
