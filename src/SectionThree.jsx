c93bdad8958ce11b89ce5077763924b7fa27c232
Raya46 <muhammadrayaarrizki@gmail.com>
2024-02-18 17:25:12 +0700

    fix gallery & wrapper sectiontwo

--- src/SectionThree.jsx
+++ src/SectionThree.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ukiran from "./assets/ukiran.png";
 import borobudur from "./assets/borobudur.png";
 import komang from "./assets/komang.png";
@@ -8,24 +8,32 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function SectionThree() {
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger)
+    gsap.registerPlugin(ScrollTrigger);
     gsap.to(".wrapper-1", {
       x: "60%",
+      ease: "power1.inOut",
+      duration: 1,
       scrollTrigger: {
         trigger: ".wrapper-trigger",
         start: "top center",
         end: "bottom center",
-        scrub: 0.3,
+        scrub: 0.1,
+        repeat: -1,
+        repeatRefresh: true,
       },
     });
 
     gsap.to(".wrapper-2", {
       x: "-60%",
+      ease: "power1.inOut",
+      duration: 1,
       scrollTrigger: {
         trigger: ".wrapper-trigger",
         start: "top center",
         end: "bottom center",
-        scrub: 0.3,
+        scrub: 0.1,
+        repeat: -1,
+        repeatRefresh: true,
       },
     });
   }, []);
@@ -37,7 +45,7 @@ export default function SectionThree() {
           <img
             src={borobudur}
             alt="none"
-            className="w-[259px] h-[122px] object-cover"
+            className="w-[259px] h-[122px] object-cover border-white border rounded-[16px]"
           />
           <span className="stroke-text">ARCHITECTURE</span>
           <img
@@ -45,13 +53,31 @@ export default function SectionThree() {
             alt="none"
             className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
           />
-          <p className="text-white text-[96px] font-bold">CULTURES</p>
+          <span className="text-white text-[96px] font-bold">CULTURES</span>
           <img
             src={ukiran}
             alt="none"
             className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
           />
-          <p className="text-white text-[96px] font-bold">ART</p>
+          <span className="text-white text-[96px] font-bold">ART</span>
+          <img
+            src={borobudur}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover border-white border rounded-[16px]"
+          />
+          <span className="stroke-text">ARCHITECTURE</span>
+          <img
+            src={komang}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <span className="text-white text-[96px] font-bold">CULTURES</span>
+          <img
+            src={ukiran}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <span className="text-white text-[96px] font-bold">ART</span>
         </div>
         <div className="wrapper-2 inline-flex flex-nowrap max-w-screen items-center gap-[32px] lg:-ml-0 -ml-[220px]">
           <img
@@ -59,19 +85,37 @@ export default function SectionThree() {
             alt="none"
             className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
           />
-          <p className="text-white text-[96px] font-bold">ART</p>
+          <span className="text-white text-[96px] font-bold">ART</span>
           <img
             src={borobudur}
             alt="none"
-            className="w-[259px] h-[122px] object-cover"
+            className="w-[259px] h-[122px] object-cover border-white border rounded-[16px]"
           />
-          <p className="stroke-text">ARCHITECTURE</p>
+          <span className="stroke-text">ARCHITECTURE</span>
           <img
             src={komang}
             alt="none"
             className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
           />
-          <p className="text-white text-[96px] font-bold">CULTURES</p>
+          <span className="text-white text-[96px] font-bold">CULTURES</span>
+          <img
+            src={ukiran}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <span className="text-white text-[96px] font-bold">ART</span>
+          <img
+            src={borobudur}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover border-white border rounded-[16px]"
+          />
+          <span className="stroke-text">ARCHITECTURE</span>
+          <img
+            src={komang}
+            alt="none"
+            className="w-[259px] h-[122px] object-cover rounded-[16px] border-white border"
+          />
+          <span className="text-white text-[96px] font-bold">CULTURES</span>
         </div>
       </div>
     </div>
